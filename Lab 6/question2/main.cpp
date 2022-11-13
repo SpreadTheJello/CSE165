18ed6af1d398ee5ca1b4aa6e7dbc49337f5736b4
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2022-11-13 13:13:12 -0800

    Lab 6 - Complete

--- Lab 6/question2/main.cpp
+++ Lab 6/question2/main.cpp
@@ -1,18 +1,18 @@
 /*
 Create a header file (say myHeader1.h) containing a 
-namespace(say myNamespace). Inside the namespacecreate 
+namespace(say myNamespace). Inside the namespace create 
 two function declarations(say fun1 and fun2). Now create 
 a second header  file (say myHeader2.h) that includes 
 the first one (i.e., include “myHeader1.h”) and continues
-the namespace, adding twomore function declarations
+the namespace, adding two more function declarations
 (say fun3 and fun4). Now create a cpp file (say main.cpp)
- that includes the second header file(i.e., include 
- “myHeader2.h”).Inside a function definition(say test1) 
- in main.cpp, call fun1 and fun2 using a scope resolution 
- operator.Inside  a  separate  function  definition
- (say test2)in  main.cpp, call  fun3  and  fun4  using a 
- using directive to your namespace. Inside main(), call 
- test1 and test2.
+that includes the second header file(i.e., include 
+“myHeader2.h”).Inside a function definition(say test1) 
+in main.cpp, call fun1 and fun2 using a scope resolution 
+operator.Inside  a  separate  function  definition
+(say test2)in  main.cpp, call  fun3  and  fun4  using a 
+using directive to your namespace. Inside main(), call 
+test1 and test2.
 */
 #include <iostream>
 #include "myHeader2.h"
